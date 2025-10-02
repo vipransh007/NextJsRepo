8a3ffffcaecf2bef4a965af3862d6ddab0864958
Vipransh <vipransh231527@gmail.com>
2025-10-02 16:20:01 +0530

    again

--- nextjs_app/app/page.tsx
+++ nextjs_app/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <h1>Please tell me one thing is this Vipransh Anand</h1>
+   <h6>Please tell me one thing is this Vipransh Anand</h6>
   );
 }
