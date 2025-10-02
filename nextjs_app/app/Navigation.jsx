2035dd96bff81a4157d02d71901d64965f9312b5
Vipransh <vipransh231527@gmail.com>
2025-10-02 19:21:50 +0530

    Link tag

---
+++ nextjs_app/app/Navigation.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navigation() {
+  return (
+    <header>
+        <div>Navigation</div>
+          <nav>
+    <ul>
+        <li><Link href="/"> Home</Link></li>
+        <li><Link href="/about"> About</Link></li>
+        <li><Link href="/service"> service</Link></li>
+    </ul>
+  </nav>
+
+    </header>
+    
+  )
+
+}
+
+export default Navigation
